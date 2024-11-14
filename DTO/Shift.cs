6b8bd4b54e74a349410ca5e7b776b4d16a91b315
2ThuanCr <2ThuanCr03@gmail.com>
2024-11-14 10:36:18 +0700

    Tạo form phân ca, sửa ca

---
+++ DTO/Shift.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Shift
+    {
+        public string MaCa { get; set; }
+        public string MaNV { get; set; }
+        public DateTime NgayLam { get; set; }
+        public TimeSpan GioBatDau { get; set; }
+        public TimeSpan GioKetThuc { get; set; }
+        public string HoTenNV { get; set; }
+    }
+
+}
---
+++ GUI/FSuaCa.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace GUI
+{
+    partial class FSuaCa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dtpNgayLam = new System.Windows.Forms.DateTimePicker();
+            this.lblNgayLam = new System.Windows.Forms.Label();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.lblMaNV = new System.Windows.Forms.Label();
+            this.txtMaCa = new System.Windows.Forms.TextBox();
+            this.lblMaCa = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Red;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnCancel.Location = new System.Drawing.Point(239, 200);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(114, 38);
+            this.btnCancel.TabIndex = 15;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Blue;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSave.Location = new System.Drawing.Point(37, 200);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(114, 38);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // dtpNgayLam
+            // 
+            this.dtpNgayLam.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNgayLam.Location = new System.Drawing.Point(153, 145);
+            this.dtpNgayLam.Name = "dtpNgayLam";
+            this.dtpNgayLam.Size = new System.Drawing.Size(200, 26);
+            this.dtpNgayLam.TabIndex = 13;
+            // 
+            // lblNgayLam
+            // 
+            this.lblNgayLam.AutoSize = true;
+            this.lblNgayLam.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNgayLam.Location = new System.Drawing.Point(21, 151);
+            this.lblNgayLam.Name = "lblNgayLam";
+            this.lblNgayLam.Size = new System.Drawing.Size(93, 20);
+            this.lblNgayLam.TabIndex = 12;
+            this.lblNgayLam.Text = "Ngày Làm:";
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(153, 74);
+            this.txtMaNV.Multiline = true;
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(200, 33);
+            this.txtMaNV.TabIndex = 11;
+            // 
+            // lblMaNV
+            // 
+            this.lblMaNV.AutoSize = true;
+            this.lblMaNV.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaNV.Location = new System.Drawing.Point(21, 87);
+            this.lblMaNV.Name = "lblMaNV";
+            this.lblMaNV.Size = new System.Drawing.Size(126, 20);
+            this.lblMaNV.TabIndex = 10;
+            this.lblMaNV.Text = "Mã Nhân Viên:";
+            // 
+            // txtMaCa
+            // 
+            this.txtMaCa.Location = new System.Drawing.Point(153, 27);
+            this.txtMaCa.Name = "txtMaCa";
+            this.txtMaCa.Size = new System.Drawing.Size(200, 26);
+            this.txtMaCa.TabIndex = 9;
+            // 
+            // lblMaCa
+            // 
+            this.lblMaCa.AutoSize = true;
+            this.lblMaCa.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaCa.Location = new System.Drawing.Point(21, 33);
+            this.lblMaCa.Name = "lblMaCa";
+            this.lblMaCa.Size = new System.Drawing.Size(65, 20);
+            this.lblMaCa.TabIndex = 8;
+            this.lblMaCa.Text = "Mã Ca:";
+            // 
+            // FSuaCa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(406, 285);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dtpNgayLam);
+            this.Controls.Add(this.lblNgayLam);
+            this.Controls.Add(this.txtMaNV);
+            this.Controls.Add(this.lblMaNV);
+            this.Controls.Add(this.txtMaCa);
+            this.Controls.Add(this.lblMaCa);
+            this.Name = "FSuaCa";
+            this.Text = "FSuaCa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DateTimePicker dtpNgayLam;
+        private System.Windows.Forms.Label lblNgayLam;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.Label lblMaNV;
+        private System.Windows.Forms.TextBox txtMaCa;
+        private System.Windows.Forms.Label lblMaCa;
+    }
+}
